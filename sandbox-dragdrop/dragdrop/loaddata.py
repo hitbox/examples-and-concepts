9993aacf508795eb196e30c5de9b8639454abdac
Carl Harris <hitbox@ryzen.com>
2023-11-18 03:54:21 -0500

    dragdrop: remove unused functions

--- sandbox-dragdrop/dragdrop/loaddata.py
+++ sandbox-dragdrop/dragdrop/loaddata.py
@@ -13,23 +13,9 @@ def available_trucks():
 def available_workers():
     return db.session.scalars(query.available_workers()).all()
 
-def get_dropzones():
-    return dict(
-        pads_dropzone = Dropzone(available_pads()),
-        trucks_dropzone = Dropzone(available_trucks()),
-        workers_dropzone = Dropzone(available_workers()),
-    )
-
 def get_dropzones_as_object():
     return DragDrop(
         pads = available_pads(),
         trucks = available_trucks(),
         workers = available_workers(),
     )
-
-def get_dropzones_as_list():
-    return [
-        Dropzone(available_pads()),
-        Dropzone(available_trucks()),
-        Dropzone(available_workers()),
-    ]
